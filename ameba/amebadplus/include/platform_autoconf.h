e74d29555deb47229dcb78693e440b9491fcc712
zjian zhang <zjian_zhang@realsil.com.cn>
2025-10-20 10:37:37 +0800

    [common][zeephyr] sync changes from github submit
    * sync changes from github submit

--- ameba/amebadplus/include/platform_autoconf.h
+++ ameba/amebadplus/include/platform_autoconf.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2024 Realtek Semiconductor Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #define CONFIG_AMEBADPLUS 1
 #define CONFIG_IMG1_FLASH 1
 #define CONFIG_IMG2_FLASH 1
