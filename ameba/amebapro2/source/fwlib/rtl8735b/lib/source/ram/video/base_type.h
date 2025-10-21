b74ed564e94962533975e91a1ffc46012a97d8dc
galex.fang <galex.fang@realtek.com>
2025-10-21 15:16:40 +0800

    [amebapro2] update copyright
    Change-Id: If2a994fa8343cd991df0104bc4958df259957bb8

--- ameba/amebapro2/source/fwlib/rtl8735b/lib/source/ram/video/base_type.h
+++ ameba/amebapro2/source/fwlib/rtl8735b/lib/source/ram/video/base_type.h
@@ -1,18 +1,3 @@
-/*------------------------------------------------------------------------------
---                                                                                                                               --
---       This software is confidential and proprietary and may be used                                   --
---        only as expressly authorized by a licensing agreement from                                     --
---                                                                                                                               --
---                            Verisilicon.                                                                                    --
---                                                                                                                               --
---                   (C) COPYRIGHT 2014 VERISILICON                                                            --
---                            ALL RIGHTS RESERVED                                                                    --
---                                                                                                                               --
---                 The entire notice above must be reproduced                                                 --
---                  on all copies and should not be removed.                                                    --
---                                                                                                                               --
---------------------------------------------------------------------------------*/
-
 #ifndef BASE_TYPE_H
 #define BASE_TYPE_H
 
--- ameba/amebapro2/source/fwlib/rtl8735b/lib/source/ram/video/hal_video_cmd.h
+++ ameba/amebapro2/source/fwlib/rtl8735b/lib/source/ram/video/hal_video_cmd.h
@@ -1,18 +1,3 @@
-/*------------------------------------------------------------------------------
---                                                                                                                               --
---       This software is confidential and proprietary and may be used                                   --
---        only as expressly authorized by a licensing agreement from                                     --
---                                                                                                                               --
---                            Verisilicon.                                                                                    --
---                                                                                                                               --
---                   (C) COPYRIGHT 2014 VERISILICON                                                            --
---                            ALL RIGHTS RESERVED                                                                    --
---                                                                                                                               --
---                 The entire notice above must be reproduced                                                 --
---                  on all copies and should not be removed.                                                    --
---                                                                                                                               --
---------------------------------------------------------------------------------*/
-
 #ifndef HAL_VIDEO_CMD_H
 #define HAL_VIDEO_CMD_H
 
